5f368900aded9d5d3eea7305420baf93cbfc5b6d
corry <cloudQ_Q163.com>
2020-11-20 14:47:49 +0800

    增加文件读取

--- loaderDemo.js
+++ loaderDemo.js
@@ -1,5 +1,34 @@
-module.exports = function(source) {
+const fs = require('fs')
+const path = require('path')
+const loaderUtils = require('loader-utils')
+module.exports = async function(source) {
   const result = RegExp('(@include\\()(.*?)\\)').exec(source)
-  console.log(result)
-  return source
+  let filePath = result[2] // 获取需要插入的文件路径 需要把它处理成绝对路径
+  const request = loaderUtils.urlToRequest(filePath)
+  const url = await resolvePath(this, path.resolve(__dirname, 'src'), request)
+  const res = await readFile(url, {encoding: "utf8"})
+  return source.replace(result[0], res)
+}
+function readFile(url, options) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(url, options, (err, data)=>{
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+      
+    })
+  })
+}
+function resolvePath(_this, fullPath, request) {
+  return new Promise((resolve, reject) => {
+    _this.resolve(fullPath, request, (err, url)=>{
+      if (err) {
+        reject(err)
+      } else {
+        resolve(url)
+      }
+    })
+  })
 }
